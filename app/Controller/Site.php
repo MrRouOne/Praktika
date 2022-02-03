29d77d4f331a63d04c21f8c53141b3d48c83014c
Кузюра Остап Дмитриевич <gr492_kod@ttit.local>
2022-02-03 13:26:23 +0700

    user validate

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -13,8 +13,7 @@ class Site
 {
     public function index(Request $request): string
     {
-        $posts = Post::where('id', $request->id)->get();
-        return (new View())->render('site.post', ['posts' => $posts]);
+        return (new View())->render('site.post');
     }
 
 
@@ -25,10 +24,17 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method === 'POST' && User::create($request->all())) {
-            app()->route->redirect('/go');
+        if ($request->method === 'GET') {
+            return new View('site.signup');
         }
-        return new View('site.signup');
+
+        if($request->method === 'POST' && User::where('login', $request->login)){
+           return new View('site.signup', ['message' => 'Логин уже существует']);
+        }
+
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/login');
+        }
     }
 
     public function login(Request $request): string
@@ -50,6 +56,4 @@ class Site
         Auth::logout();
         app()->route->redirect('/hello');
     }
-
-
 }
--- views/site/post.php
+++ views/site/post.php
@@ -1,8 +1,1 @@
-<h1>Список статей</h1>
-<ol>
-    <?php
-    foreach ($posts as $post) {
-        echo '<li>' . $post->title . '</li>';
-    }
-    ?>
-</ol>
+<h2><?= $message ?? ''; ?></h2>
